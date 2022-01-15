76525dedc35fc0a19363e56c63d367944637420f
Mark Biek <support@janustech.net>
2022-01-15 15:02:25 -0500

    Added additional user fields

---
+++ database/migrations/2022_01_15_195434_add_users_fields.php
@@ -0,0 +1,71 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddUsersFields extends Migration {
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::table('users', function (Blueprint $table) {
+			$table
+				->boolean('superadmin')
+				->nullable(false)
+				->default(false);
+			$table
+				->boolean('is_enabled')
+				->nullable(false)
+				->default(false);
+			$table
+				->string('shop_name')
+				->default('')
+				->nullable(false);
+			$table
+				->string('shop_domain')
+				->default('')
+				->nullable(false);
+			$table
+				->string('card_brand')
+				->default('')
+				->nullable(false);
+			$table
+				->string('card_last_four')
+				->default('')
+				->nullable(false);
+			$table
+				->string('billing_plan')
+				->default('')
+				->nullable(false);
+			$table->timestamp('trial_starts_at');
+			$table->timestamp('trial_ends_at');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::table('users', function (Blueprint $table) {
+			$columns = [
+				'superadmin',
+				'is_enabled',
+				'shop_name',
+				'shop_domain',
+				'card_brand',
+				'card_last_four',
+				'billing_plan',
+				'trial_starts_at',
+				'trial_ends_at',
+			];
+			foreach ($columns as $column) {
+				$table->dropColumn($column);
+			}
+		});
+	}
+}
