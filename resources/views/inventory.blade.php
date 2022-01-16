17d36e206c6b29f581fe5e16454557a55c24bb60
Mark Biek <support@janustech.net>
2022-01-16 13:44:19 -0500

    Inventory view
    The base inventory view shows how many inventory items the user has and
    lets them filter by quantity of items.
    
    Each product in the inventory has a link to view just those specific
    products.

--- resources/views/inventory.blade.php
+++ resources/views/inventory.blade.php
@@ -11,16 +11,18 @@
                 <div class="p-6 bg-white border-b border-gray-200">
 					<h1>{{ $title }}</h1>
 
-					<p class="font-semibold p-2">You have {{ $user->inventory_total }} items in your inventory.</p>
+					@if (!isset($product))
+						<p class="font-semibold p-2">You have {{ $user->inventory_total }} items in your inventory.</p>
 
-					<form class="form mt-4 pb-8" method="GET" action="">
-						@csrf
-						<div class="form-group">
-							<label for="max_quantity">Find inventory with quantity less than:</label>
-							<input type="number" name="max_quantity" id="max_quantity" value="25" />
-						</div>
-						<input type="submit" name="submit" value="Go" />
-					</form>
+						<form class="form mt-4 pb-8" method="GET" action="">
+							@csrf
+							<div class="form-group">
+								<label for="max_quantity">Find inventory with quantity less than:</label>
+								<input type="number" name="max_quantity" id="max_quantity" value="25" />
+							</div>
+							<input type="submit" name="submit" value="Go" />
+						</form>
+					@endif
 
 					@include('components.inventory.list')
                 </div>
