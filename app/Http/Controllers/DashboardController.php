7375536cfef93880066cc5232f2d768a7914ccdd
Mark Biek <support@janustech.net>
2022-01-16 11:52:26 -0500

    Basic display of user's products

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller {
 	public function show(Request $request) {
-		return view('dashboard');
+		$user = Auth::user();
+
+		return view('dashboard', [
+			'products' => $user
+				->products()
+				->orderBy('brand')
+				->orderBy('product_name')
+				->orderBy('style')
+				->get(),
+		]);
 	}
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,26 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+					<h1>Products</h1>
+
+					<table class="table-auto table-striped border-collapse mt-4">
+						<thead>
+							<tr class="text-left">
+								<th>Name</th>
+								<th>Style</th>
+								<th>Brand</th>
+							</tr>
+						</thead>
+						<tbody>
+							@foreach ($products as $product)
+								<tr>
+									<td>{{ $product->product_name }}</td>
+									<td>{{ $product->style }}</td>
+									<td>{{ $product->brand }}</td>
+								</tr>
+							@endforeach
+						</tbody>
+					</table>
                 </div>
             </div>
         </div>
