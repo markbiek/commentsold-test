45a1ad1d68aef8e547a70ec352565f66b659673b
Mark Biek <support@janustech.net>
2022-01-16 13:44:12 -0500

    Refactored product layout into subviews

---
+++ resources/views/components/products/list.blade.php
@@ -0,0 +1,16 @@
+<table class="table-auto table-striped border-collapse mt-4">
+	<thead>
+		<tr class="text-left">
+			<th></th>
+			<th>Name</th>
+			<th>Style</th>
+			<th>Brand</th>
+			<th></th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach ($products as $product)
+			<livewire:edit-product-row :product="$product" />
+		@endforeach
+	</tbody>
+</table>
